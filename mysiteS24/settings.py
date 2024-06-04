f4ffda41618fd8e6e80ecfc3528af3a6b6848edf
Nana <nanasmacbookprowithtouchbar@songruohandeMacBook-Pro.local>
2024-06-04 18:32:22 -0400

    Nana|add several views

--- mysiteS24/settings.py
+++ mysiteS24/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-l$)xn8se+^_+!&2h$@zo=#%i1x+$-167g=o!5k^jta(@c7@cyv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- mysiteS24/urls.py
+++ mysiteS24/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from myapp.views import home_view, about_view
 
@@ -25,4 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home_view, name="home"),
     path("about/", about_view, name="about"),
+    path(r'myapp/', include('myapp.urls1')),
 ]
